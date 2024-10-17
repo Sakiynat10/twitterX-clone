245408c4c7a9067e9228824b869722b2fd1e66b5
Sakiynat10 <xoliyorvasilbek4@gmail.com>
2024-10-17 13:43:05 +0500

    login register

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
-// import { authOptions } from "@/lib/auth-options";
+// `import { authOptions } from "@/lib/auth-options";
 // import NextAuth from "next-auth/next";
 
 // const handler = NextAuth(authOptions)
-// export {handler as GET, handler as POST};
+// export {handler as GET, handler as POST};`
